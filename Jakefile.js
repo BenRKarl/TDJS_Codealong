6d44140a78906ee079072160c11763f3171b7d95
Ben Karl <bkarl@cyrusinnovation.com>
2015-04-01 15:28:30 -0400

    now linting all .js files not included in node_modules

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,19 @@
+/* global desc, task, jake, fail, complete */
+
+'use strict';
+
 task('default', ['lint']);
 
 desc('lint everything');
 task('lint', [], function(){
-	var lint = require('./lint_runner.js');
-	lint.validateFile('Jakefile.js', {}, {});
+	var lint  = require('./lint_runner.js');
+	var files = new jake.FileList();
+	files.include('**/*.js');
+	files.exclude('node_modules');
+
+	var options = {
+		node: true
+	};
+
+	lint.validateFileList(files.toArray(), options, {});
 });
