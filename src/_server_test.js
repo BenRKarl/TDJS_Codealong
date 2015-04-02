2356b41927c6736f9eb9e01d55fe12e7a7fdc283
Ben Karl <bkarl@cyrusinnovation.com>
2015-04-02 18:05:23 -0400

    basic hello world server

--- src/_server_test.js
+++ src/_server_test.js
@@ -17,4 +17,25 @@ exports.testServerRespondsToGetRequests = function(test) {
     test.done();
   });
 };
- 
+
+exports.test_serverReturnsHelloWorld = function(test) {
+  server.start();
+
+  var request = http.get('http://localhost:8080');
+  request.on('response', function(response) {    
+    var receivedData = false;
+    response.setEncoding('utf8');
+
+    test.equals(200, response.statusCode, 'status code');
+
+    response.on('data', function(chunk){
+      receivedData = true;
+      test.equals('Hello World', chunk, 'response text');
+    });
+
+    response.on('end', function() {
+      test.ok(receivedData, 'should have received response data');
+      test.done();
+    });
+  });
+};
